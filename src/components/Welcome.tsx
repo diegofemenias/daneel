dfaa62433898195edaa751d391204024ba0816c4
Diego FG <femenias@gmail.com>
2024-12-11 12:35:48 -0300

    Update Welcome.tsx

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -1,15 +1,9 @@
 export const Welcome: React.FC = () => (
   <div className="bg-white border-gray-100 border-2 rounded-lg px-8 py-5 mr-20 w-full">
-    <h1 className="text-2xl font-bold mb-2">🤖 Hello, I am Daneel</h1>
+    <h1 className="text-2xl font-bold mb-2">🤖 ¡Hola!, soy el profe</h1>
     <p>
-      You can use me to create your own chat bot app. In this demo I am an
-      expert in movie reviews, so feel free to ask me about movies! Then, create
-      your own bot in under five minutes.
-    </p>
-    <p>
-      <a className="underline" href="https://github.com/ascorbic/daneel#readme">
-        Build your own
-      </a>
+      Soy un profesor universitario de Sistemas.  Podés usarme como apoyo a tus consultas académicas ya que fuí entrenado específicamente para ello. Lamentablemente no puedo responder consultas de otros temas
+      ya que no cuento con conocimientos fiables.  Si querés saber de otros temas, consultá a mi primo <a href="https://chat.openai.com" className="underline">ChatGPT</a>.
     </p>
   </div>
 );
